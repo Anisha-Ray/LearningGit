c5097a502fc8ee2c784addb040a6e56a8f8553e3
Anisha <anisharay2610@gmail.com>
2022-06-03 11:55:04 +0530

    second commit

--- Myfirstjavaprogram.java
+++ Myfirstjavaprogram.java
@@ -1,6 +1,7 @@
 class Myfirstjavaprogram{
     public static void main(String[]args){
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+        System.out.print("second file");
     }
 }
       
